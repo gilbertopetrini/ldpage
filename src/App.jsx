aeb28ea4f78f698b3d602351099580a1cffbaab7
petrinidias <petrini4u@gmail.com>
2025-06-26 20:29:24 -0300

    ldpage

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react'
+import './App.css'
+import { MdArrowOutward } from "react-icons/md";
+import { FaCheck } from "react-icons/fa";
+import hand from './assets/hand.png';
+import { PiSmileyWink } from "react-icons/pi";
+
+function App() {
+
+  return (
+    <div className='main'>
+      <p className='title'>Faça parte da <span className="glow">ostentação</span> <br/>do mercado digital.</p>
+
+      <div className='card1'>
+
+        <div className='maispopular'>
+          Mais popular
+        </div>
+
+        <div className='maincontainer'>
+          <div className='header'>
+            <p>Com gerador de notificações</p>
+            <p>Pagamento único para sempre.</p>
+          </div>
+
+          <div className='mid1'>
+              <p>4x</p>
+              <p className='price'>R$8,28</p>
+              <div className='mid1info'>
+                <p>ou <strong>R$29,90</strong> no PIX</p>
+                <p>pagamento único</p>
+              </div>
+          </div>
+
+          <a className='mid2' href="https://pay.kirvano.com/f35fbd4d-5c78-49b7-99ba-2ddaf04e8e9c" target='_blank'>
+            Comprar agora <MdArrowOutward/>
+          </a>
+
+          <div className='footer'>
+
+            <div className='topic'>
+              <p className='check'><FaCheck /></p><p>Seu acesso para sempre!</p>
+            </div>
+
+            <div className='topic'>
+              <p className='check'><FaCheck /></p><p>Gerador de notificações</p>
+            </div>
+
+            <div className='topic'>
+              <p className='check'><FaCheck /></p><p>Login garantido por toda a vida</p>
+            </div>
+
+            <div className='topic'>
+              <p className='check'><FaCheck /></p><p>Atualizações gratuitas e contínuas</p>
+            </div>
+
+          </div>
+        
+        </div>
+
+      </div>
+      
+
+
+
+      <div className='card1'>
+
+        <div className='maincontainer2'>
+          <div className='header'>
+            <p>Sem gerador de notificações</p>
+            <p>Pagamento único para sempre.</p>
+          </div>
+
+          <div className='mid1'>
+              <p>4x</p>
+              <p className='price'>R$6,62</p>
+              <div className='mid1info'>
+                <p>ou <strong>R$23,90</strong> no PIX</p>
+                <p>pagamento único</p>
+              </div>
+          </div>
+
+          <a className='mid2' href="https://pay.kirvano.com/2010147e-3036-47c6-8a25-9fa54c6e8d30" target='_blank'>
+            Comprar agora <MdArrowOutward/>
+          </a>
+
+          <div className='footer'>
+
+            <div className='topic'>
+              <p className='check'><FaCheck /></p><p>Seu acesso para sempre!</p>
+            </div>
+
+            <div className='topic'>
+              <p className='check'><FaCheck /></p><p>Login garantido por toda a vida</p>
+            </div>
+
+            <div className='topic'>
+              <p className='check'><FaCheck /></p><p>Atualizações gratuitas e contínuas</p>
+            </div>
+
+          </div>
+        
+        </div>
+
+      </div>
+
+      <footer>
+        Este aplicativo é apenas uma simulação para brincadeiras e fins cinematográficos. Não gera valores reais nem pode ser usado para transações verdadeiras ou falsas.
+      </footer>
+    </div>
+  )
+}
+
+export default App
